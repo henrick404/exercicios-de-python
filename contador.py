5dcd2bd906f69858f5448c8658a0ff01a743852c
henrick404 <160244828+henrick404@users.noreply.github.com>
2025-04-21 15:29:41 -0300

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,11 @@
+contador = 0
+while True:
+    print(contador," ", end=" ")
+    usuario=str(input('Deseja incrementar uma unidade? Y/N '))
+    
+    if usuario == "N":
+        break
+    elif usuario == "Y":
+        contador+=1
+    else:
+        print("responde direito")
